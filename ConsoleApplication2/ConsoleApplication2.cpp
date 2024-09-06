85b102324cde7e4b0f5a4e536aefbfd7e731e0ca
NameLess491 <dmitriy.mitnik99@gmail.com>
2024-09-06 18:51:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,43 @@
+﻿mkdir cpp - calculator
+cd cpp - calculator
+git init
+git remote add origin https ://github.com/NameLess491/cpp-calculator.git
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (operation) {
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0)
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+        else
+            cout << "Division by zero is not allowed!" << endl;
+        break;
+    default:
+        cout << "Invalid operator!" << endl;
+    }
+
+    return 0;
+}
+git add main.cpp
+git commit - m "Initial commit: added simple calculator"
+git push - u origin master
